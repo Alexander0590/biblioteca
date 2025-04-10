91e56e91686efe2973aa0a78b696cf2344ea33f4
Alexander Dev <josealexanderyoverasimbala@gmail.com>
2025-04-10 15:42:12 -0500

    version 1.0

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="lib/boostrap-css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/boostrap-iconos/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/estilosindex.css">
+</head>
+<body>
+
+</head>
+<body>
+
+<div class="login-card">
+    <img src="img/logo.png" alt="Logo Biblioteca" id="img1">
+    <h3 class="login-title">Ingreso Biblioteca</h3>
+    <form>
+      <div class="mb-3 text-start">
+        <label for="usuario" class="form-label">Usuario</label>
+        <input type="text" class="form-control" id="usuario" placeholder="Usuario">
+      </div>
+      <div class="mb-3 text-start">
+        <label for="contrasena" class="form-label">Contraseña</label>
+        <input type="password" class="form-control" id="contrasena" placeholder="********">
+      </div>
+      <div class="d-grid">
+        <button type="submit" class="btn btn-login">Iniciar sesión</button>
+      </div>
+    </form>
+  </div>
+
+<script src="lib/boostrap-js/bootstrap.min.js"></script>
+</body>
+</html>
